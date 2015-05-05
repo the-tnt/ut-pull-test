914e2376e95c5849dd4c283de21fb35e1527fee0
Oleg Iasinovskyi <tnt@sjinnovation.com>
2015-05-05 17:27:56 +0300

    Add schema file

---
+++ app/Config/Schema/schema.php
@@ -0,0 +1,32 @@
+<?php
+class AppSchema extends CakeSchema {
+
+    public function before($event = array()) {
+        return true;
+    }
+
+    public function after($event = array()) {
+    }
+
+    public $groups = array(
+        'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 10, 'unsigned' => true, 'key' => 'primary'),
+        'title' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 50, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+        'type' => array('type' => 'string', 'null' => false, 'default' => 'auto', 'length' => 6, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+        'indexes' => array(
+            'PRIMARY' => array('column' => 'id', 'unique' => 1)
+        ),
+        'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
+    );
+
+    public $users = array(
+        'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 10, 'unsigned' => true, 'key' => 'primary'),
+        'email' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 100, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+        'password' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 100, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+        'created' => array('type' => 'datetime', 'null' => false, 'default' => null),
+        'indexes' => array(
+            'PRIMARY' => array('column' => 'id', 'unique' => 1)
+        ),
+        'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
+    );
+
+}
