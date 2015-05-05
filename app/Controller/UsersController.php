f4ad43e8d7841876ade083410f0101118349ef30
Oleg Iasinovskyi <tnt@sjinnovation.com>
2015-05-05 18:48:35 +0300

    Changes

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -23,7 +23,9 @@ class UsersController extends AppController {
  */
     public function index() {
         $this->User->recursive = 0;
-        $this->set('users', $this->Paginator->paginate());
+        $users = $this->Paginator->paginate();
+
+        $this->set('users', $users);
     }
 
 /**
