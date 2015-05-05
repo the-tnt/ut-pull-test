73d21b3cef98d7ada04ecdc0fda674cd357ad717
Oleg Iasinovskyi <tnt@sjinnovation.com>
2015-05-05 17:26:20 +0300

    Add group model

---
+++ app/Model/Group.php
@@ -0,0 +1,15 @@
+<?php
+App::uses('AppModel', 'Model');
+/**
+ * Group Model
+ *
+ */
+class Group extends AppModel {
+
+/**
+ * Display field
+ *
+ * @var string
+ */
+    public $displayField = 'title';
+}
