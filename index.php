ab3139b335e3b6216095b11a8229be7bddd00958
Oleg Iasinovskyi <tnt@sjinnovation.com>
2015-06-16 16:52:08 +0300

    e1

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
-/**
+/**1
  * Requests collector.
- *
+ * 
  *  This file collects requests if:
  *	- no mod_rewrite is available or .htaccess files are not supported
  *  - requires App.baseUrl to be uncommented in app/Config/core.php
